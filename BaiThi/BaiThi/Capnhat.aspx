5c425b49040132573ca5804237fe16abe036ae2f
NguyenThiNgocTram122 <ngoctram201715@gmail.com>
2023-12-22 20:17:20 +0700

    dfsf

---
+++ BaiThi/BaiThi/Capnhat.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Masterpage.Master" CodeBehind="Capnhat.aspx.cs" Inherits="BaiThi.Capnhat" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+     <div>
+                 <asp:Label ID="Label3" runat="server" Text="DANH SÁCH MÔN HỌC "></asp:Label>
+            </div>
+           <div>
+               <asp:Label ID="Label2" runat="server" Text="Mã môn học"></asp:Label>
+               <asp:TextBox ID="txtmamonhoc" runat="server"></asp:TextBox>
+           </div>
+            <div>
+                <asp:Label ID="Label4" runat="server" Text="Tên môn học"></asp:Label>
+                <asp:TextBox ID="txttenmonhoc" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" PageSize="3">
+                    <AlternatingRowStyle BackColor="White" />
+                    <Columns>
+                        <asp:BoundField DataField="MaMH" HeaderText="Mã môn học" />
+                        <asp:BoundField DataField="TenMH" HeaderText="Tên môn học" />
+                        <asp:CommandField SelectText="Chọn" ShowSelectButton="True" />
+                    </Columns>
+                    <EditRowStyle BackColor="#2461BF" />
+                    <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                    <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                    <RowStyle BackColor="#EFF3FB" />
+                    <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                    <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                    <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                    <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                    <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                </asp:GridView>
+            </div>
+            <div>
+                <asp:Label ID="lbtthongbao" runat="server" Text="Label"></asp:Label>
+            </div>
+            <div>
+                <asp:Button ID="btnthem" runat="server" Text="Thêm" OnClick="btnthem_Click" />
+                <asp:Button ID="btnluu" runat="server" Text="Lưu" OnClick="btnluu_Click" />
+                <asp:Button ID="btnsua" runat="server" Text="Sửa" OnClick="btnsua_Click" />
+                <asp:Button ID="btnxoa" runat="server" Text="Xóa" OnClick="btnxoa_Click" />
+            </div>
+</asp:Content>
---
+++ BaiThi/BaiThi/Capnhat.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BaiThi
+{
+    public partial class Capnhat : System.Web.UI.Page
+
+    {
+        public static string tram = "Data Source=LAPTOP-00N9N4JN\\SQLEXPRESS;Initial Catalog=QL_SINHVIEN;Integrated Security=True";
+        public static SqlConnection cn = new SqlConnection(tram);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi(); 
+        }
+        void HienThi()
+        {
+            try
+            {
+                string chuoiSQL = "SELECT * FROM tbl_Monhoc";
+                SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+
+                GridView1.DataBind();
+            }
+            catch (Exception)
+            {
+                lbtthongbao.Text = "Lỗi kết nối";
+            }
+        }
+        void ThucThi(string caulenh)
+        {
+            SqlCommand cm = new SqlCommand(caulenh, cn);
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+        }
+        Boolean kiemtra(string caulenh)
+        {
+            SqlDataAdapter sql = new SqlDataAdapter(caulenh, cn);
+            DataTable data = new DataTable();
+            sql.Fill(data);
+            if (data.Rows.Count > 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        protected void btnthem_Click(object sender, EventArgs e)
+        {
+            txtmamonhoc.Text = "";
+            txttenmonhoc.Text = "";
+        }
+        protected void btnluu_Click(object sender, EventArgs e)
+        {
+            string khaibao = "select * from tbl_monhoc where MaMH = '" + txtmamonhoc.Text + "' or TenMH = N'" + txttenmonhoc.Text + "'";
+
+            if (kiemtra(khaibao))
+            {
+
+                lbtthongbao.Text = "Tên môn học đã tồn tại";
+            }
+            else
+            {
+
+                string them = "insert into tbl_monhoc values ('" + txtmamonhoc.Text + "' , N'" + txttenmonhoc.Text + "')";
+                ThucThi(them);
+                HienThi();
+            }
+        }
+        protected void btnsua_Click(object sender, EventArgs e)
+        {
+            string sua = "update tbl_monhoc set TenMH = N'" + txttenmonhoc.Text + "' where MaMH = '" + txtmamonhoc.Text + "'";
+            ThucThi(sua);
+            HienThi();
+        }
+        protected void btnxoa_Click(object sender, EventArgs e)
+        {
+            string truyen = "delete tbl_monhoc where MaMH = '" + txtmamonhoc.Text + "'";
+            ThucThi(truyen);
+            HienThi();
+            txtmamonhoc.Text = "";
+            txttenmonhoc.Text = "";
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+        }
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string ngoctram = "select * from tbl_monhoc";
+            SqlDataAdapter sqldata = new SqlDataAdapter(ngoctram, cn);
+            DataTable dttable = new DataTable();
+            sqldata.Fill(dttable);
+            int dong = GridView1.SelectedIndex;
+            int page = GridView1.PageIndex;
+            txtmamonhoc.Text = dttable.Rows[page * 3 + dong][0].ToString();
+            txttenmonhoc.Text = dttable.Rows[page * 3 + dong][1].ToString();
+        }
+    }
+}
